cb66e48c4319e1bf37ccb4536cac87a3477a0c2f
Oksana Drul <Drul>
2020-03-21 21:00:51 +0200

    [BE] Add jsonwebtoken

--- routes/api/users.js
+++ routes/api/users.js
@@ -55,10 +55,25 @@ router.post('/', [
       user.password = await bcrypt.hash(password, salt);
 
       await user.save();
-      // Return jsonwebtoken
 
-      console.log(req.body);
-      res.send('Users route')
+      // Return jsonwebtoken
+      const payload = {
+        user: {
+          id: user.id
+        }
+      };
+
+      jwt.sign(
+        payload,
+        config.get('jwtSecret'),
+        { expiresIn: 360000 },
+        (err, token) => {
+          if (err) throw err;
+          res.json({ token });
+        }
+      )
+
+
     } catch (err) {
       console.error(err.message);
       res.status(500).send('Server error');
