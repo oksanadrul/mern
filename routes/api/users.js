d2fab660aec40427270c63d6f21da28d3859b761
Oksana Drul <Drul>
2020-03-23 18:32:00 +0200

    [BE] Get all users. Delete Profile & User

--- routes/api/users.js
+++ routes/api/users.js
@@ -79,4 +79,18 @@ router.post('/', [
     }
   });
 
+// @route    GET api/users
+// @desc     Get all users
+// @access   Public
+
+router.get('/', async (req, res) => {
+  try {
+    const users = await User.find().populate('user', ['name', 'avatar']);
+    res.json(users);
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send('Server Error')
+  }
+})
+
 module.exports = router;
