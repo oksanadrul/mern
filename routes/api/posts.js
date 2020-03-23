b07a07d116091032ce9c8b90c26f973280c233fe
Oksana Drul <Drul>
2020-03-23 23:58:32 +0200

    [BE] Add & Remove Comment Routes

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -158,4 +158,76 @@ router.put('/unlike/:id', auth, async (req, res) => {
   }
 })
 
+// @route    POST api/posts/comment/:id
+// @desc     Comment on a post
+// @access   Private
+
+router.post('/comment/:id', [auth,
+  [
+    check('text', 'Text is required').not().isEmpty()
+  ]
+], async (req, res) => {
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    return res.status(400).json({ errors: errors.array() })
+  }
+
+  try {
+    const user = await User.findById(req.user.id).select('-password');
+    const post = await Post.findById(req.params.id);
+
+    const newComment = new Post({
+      text: req.body.text,
+      name: user.name,
+      avatar: user.avatar,
+      user: req.user.id
+    });
+
+    post.comments.unshift(newComment);
+
+    await post.save();
+
+
+    res.json(post.comments);
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send('Server Error');
+  }
+});
+
+// @route    Delete api/posts/comment/:id/:comment_id
+// @desc     Delete comment
+// @access   Private
+
+router.delete('/comment/:id/:comment_id', auth, async (req, res) => {
+  try {
+    const post = await Post.findById(req.params.id);
+
+    // Pull out comment
+    const comment = post.comments.find(comment => comment.id === req.params.comment_id);
+
+    // Make sure comments exists
+    if (!comment) {
+      return res.status(404).json({ msg: 'Comment does not exists' })
+    }
+
+    // Check user
+    if (comment.user.toString() !== req.user.id) {
+      return res.status(401).json({ msg: 'User not authorized' })
+    }
+
+    post.comments = post.comments.filter(
+      ({ id }) => id !== req.params.comment_id
+    );
+
+    await post.save();
+
+    res.json(post.comments);
+
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send('Server Error');
+  }
+})
+
 module.exports = router;
