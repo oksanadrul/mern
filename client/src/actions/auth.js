79dfc3b11050e0eac899b765ef2db991540fa377
Oksana Drul <Drul>
2020-03-26 21:14:04 +0200

    [FE] Attach user profile to dashboard

--- client/src/actions/auth.js
+++ client/src/actions/auth.js
@@ -1,7 +1,7 @@
 
 import axios from 'axios';
 
-import { REGISTER_SUCCESS, REGISTER_FAIL, USER_LOADED, AUTH_ERROR, LOGIN_SUCCESS, LOGIN_FAIL, LOGOUT } from './types';
+import { REGISTER_SUCCESS, REGISTER_FAIL, USER_LOADED, AUTH_ERROR, LOGIN_SUCCESS, LOGIN_FAIL, LOGOUT, CLEAR_PROFILE } from './types';
 import { setAuthToken } from '../utils';
 import { handleServerErrors } from './handleServerErrors';
 
@@ -88,6 +88,7 @@ export const login = (email, password) => async dispatch => {
 // Logout /Clear Profile
 export const logout = () => dispatch => {
   dispatch({ type: LOGOUT })
+  dispatch({ type: CLEAR_PROFILE })
 }
 
 
--- client/src/components/dashboard/Dashboard.js
+++ client/src/components/dashboard/Dashboard.js
@@ -1,17 +1,32 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, Fragment } from 'react'
 import PropTypes from 'prop-types'
+import { Link } from 'react-router-dom'
 import { connect } from 'react-redux'
+import Spinner from '../layout/Spinner';
 import { getCurrentUserProfile } from '../../actions/profile';
 
-const Dashboard = ({ getCurrentUserProfile, auth, profile }) => {
+const Dashboard = ({
+  getCurrentUserProfile,
+  auth: { user },
+  profile: { profile, loading }
+}) => {
   useEffect(() => {
     getCurrentUserProfile();
   }, [])
-  return (
-    <div>
-      Dashboard
-    </div>
-  )
+
+  return loading && !profile ? <Spinner /> : <Fragment>
+    <h1 className='large text-primary'>Dashboard</h1>
+    <p className='lead'>
+      <i className='fas fa-user' /> Welcome {user && user.name}
+    </p>
+    {profile ? <Fragment>Has profile</Fragment> :
+      <Fragment>
+        <p>You have not yet setup a profile, please add some info</p>
+        <Link to='/create-profile' className='btn btn-primary my-1'>
+          Create Profile
+          </Link>
+      </Fragment>}
+  </Fragment>
 }
 
 Dashboard.propTypes = {
