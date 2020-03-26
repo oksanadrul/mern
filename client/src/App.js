084b428a0058115554cf1db66fd9f9bd0ff882e3
Oksana Drul <Drul>
2020-03-26 17:42:08 +0200

    [FE] Add protected Routes logic for Dashboard page

--- client/src/App.js
+++ client/src/App.js
@@ -6,11 +6,13 @@ import Register from './components/auth/Register';
 import Landing from './components/layout/Landing';
 import Navbar from './components/layout/Navbar';
 import Alert from './components/layout/Alert';
+import Dashboard from './components/dashboard/Dashboard';
+import PrivateRoute from './components/routing/PrivateRoute';
 // Redux
 import { Provider } from 'react-redux';
 import store from './store';
 import { loadUser } from './actions/auth';
-import { setAuthToken } from './utils'
+import { setAuthToken } from './utils';
 
 if (localStorage.token) {
   setAuthToken(localStorage.token);
@@ -32,6 +34,7 @@ const App = () => {
             <Switch>
               <Route exact path="/register" component={Register} />
               <Route exact path="/login" component={Login} />
+              <PrivateRoute exact path="/dashboard" component={Dashboard} />
             </Switch>
           </section>
         </Fragment>
